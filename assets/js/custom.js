730b04ec03b3b93b44c8e7c3d1eec815cef1149b
ienming <hsieh.alan.0726@gmail.com>
2021-04-09 11:44:32 +0800

    update
    update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,9 +1,9 @@
 (function () {
     // buttons
-    new Vue({
-        el: "#buttons",
+    let vm = new Vue({
+        el: "#controller",
         data: {
-            buttons: [
+            attitudeBtns: [
                 {
                     title: "Aggressive",
                     type: "aggressive",
@@ -41,6 +41,29 @@
                     type: "hurtHope",
                     class: "show"
                 }
+            ],
+            subjectBtns: [
+                {
+                    title: "You",
+                    type: 1,
+                    class: "show"
+                }, {
+                    title: "Me",
+                    type: 2,
+                    class: "show"
+                }, {
+                    title: "You and I",
+                    type: 3,
+                    class: "show"
+                }, {
+                    title: "We",
+                    type: 4,
+                    class: "show"
+                }, {
+                    title: "He/She/They",
+                    type: 5,
+                    class: "show"
+                }
             ]
         },
         methods: {
@@ -55,13 +78,60 @@
                         }
                     }
                 }
-                
-                if(this._data.buttons[idx].class == "show"){
-                    this._data.buttons[idx].class = undefined
-                }else if (this._data.buttons[idx].class == undefined){
-                    this._data.buttons[idx].class = "show"
+
+                if (this._data.attitudeBtns[idx].class == "show") {
+                    this._data.attitudeBtns[idx].class = undefined
+                } else if (this._data.attitudeBtns[idx].class == undefined) {
+                    this._data.attitudeBtns[idx].class = "show"
                 }
+
+                atudControl = 0;
+                this.attitudeBtns.forEach(e => {
+                    if (e.class == "show") {
+                        atudControl++;
+                    } else {
+                        atudControl += 0;
+                    }
+                });
+                console.log(atudControl);
+            },
+            filterSubject: function (idx, key) {
+                for (let i = 0; i < all_data_arr.length; i++) {
+                    for (let j = 0; j < all_data_arr[i].length; j++) {
+                        let nowData = all_data_arr[i][j];
+                        if (nowData.person == key && nowData.subjectShow) {
+                            nowData.subjectShow = false;
+                        } else if (nowData.person == key && !nowData.subjectShow) {
+                            nowData.subjectShow = true;
+                        }
+                    }
+                }
+
+                if (this._data.subjectBtns[idx].class == "show") {
+                    this._data.subjectBtns[idx].class = undefined
+                } else if (this._data.subjectBtns[idx].class == undefined) {
+                    this._data.subjectBtns[idx].class = "show"
+                }
+
+                subjectControl = 0;
+                this.subjectBtns.forEach(e => {
+                    if (e.class == "show") {
+                        subjectControl++;
+                    } else {
+                        subjectControl += 0;
+                    }
+                });
+                console.log(subjectControl)
             }
+        }
+    });
+
+    let footerVm = new Vue({
+        el: "#footer",
+        data: {
+            txt: `
+                Lyrics from KKBOX 年度風雲榜
+            `
         }
     });
 }());
